0ea0bbdedd0bf5261b40be9268a778ba2281fc05
Nick Petosa <nick.petosa@gmail.com>
2017-03-21 18:42:14 -0400

    Changed to simple english wikipedia, added hot links for ASL translations.

--- js/exposition.js
+++ js/exposition.js
@@ -1,22 +1,104 @@
+function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+function sanitize(word) {
+  word = replaceAll(word, ".", "")
+  word = replaceAll(word, ",", "")
+  word = replaceAll(word, "\"", "")
+  word = replaceAll(word, "\'", "")
+  word = replaceAll(word, ";", "")
+  word = replaceAll(word, ":", "")
+  word = replaceAll(word, "/", "")
+  word = replaceAll(word, "\\", "")
+  word = replaceAll(word, "?", "")
+  word = replaceAll(word, "!", "")
+  word = replaceAll(word, "~", "")
+  word = replaceAll(word, "\`", "")
+  word = replaceAll(word, "(", "")
+  word = replaceAll(word, ")", "")
+  word = replaceAll(word, "[", "")
+  word = replaceAll(word, "]", "")
+  word = replaceAll(word, "“", "")
+  word = replaceAll(word, "”", "")
+  word = replaceAll(word, "'", "")
+    return word;
+}
+
+function signify(passage) {
+    $("#result").html("");
+    passage = passage.split(" ");
+    str = ""
+    total = passage.length;
+    publish = new Array(total);
+
+    for(count = 0; count < total; count++){
+        cleaned = sanitize(passage[count]);
+
+
+        $.ajax({
+          url: "https://dictionary-smartsign.rhcloud.com/videos?keywords=" + cleaned,
+          method: 'GET',
+          dataType: 'jsonp',
+          async: false,
+          more: passage[count],
+          clean: cleaned,
+          rank: count
+        }).then(function(data) {
+          res = [];
+          data.data.forEach(function(element) {
+              res.push({"title":element.title, "thumb":element.thumbnailStandard, "youtube":element.id});
+          });
+          if (data.total > 0) {
+             out = "<span onclick=\"modal('" + this.clean + "', '" + replaceAll(JSON.stringify(res), "\"", "_DQUOTE_") + "')\" class='box tooltip' style='cursor:pointer;' > "
+             + this.more + "  <span class='tooltiptext' style='font-size: 10pt;'>How to sign \'" + this.clean + "\'</span></span>";
+          } else {
+            out = this.more + " ";
+          }
+          publish[this.rank] = out;
+          $("#result").html(publish.join(" "));
+        });
+
+
+    }
+}
+
+function modal(clean, data) {
+  obj = JSON.parse(replaceAll(data, "_DQUOTE_", "\""));
+  var modal = document.getElementById('myModal');
+  modal.style.display = "block";
+  $("#modal-list").html("")
+  $("#modal-title").html(clean)
+  obj.forEach(function(element) {
+    //$("#modal-list").append("<iframe width=\"952\" height=\"536\" src=\"https://www.youtube.com/embed/" + element.youtube + "\" frameborder=\"0\" allowfullscreen></iframe>");
+    $("#modal-list").append("<iframe src='http://www.youtube.com/embed/" + element.youtube + "' width=\"800\" height=\"450\" frameborder=\"0\"></iframe>");
+  });
+
+}
+
 function extract(noun) {
 
-
+    $("#img-result").html("");
     $.ajax({
-      url: "https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=" + noun,
+      url: "https://simple.wikipedia.org/w/api.php?action=query&prop=extracts&redirects=1 &format=json&origin=*&exintro=&titles=" + noun,
       method: 'GET',
       dataType: 'json',
-      async: false
+      async: true
     }).then(function(data) {
       res = (data.query.pages[Object.keys(data.query.pages)[0]].extract);
       if (!res) {
         res = "No results found!"
         $("#result").html(res);
       } else {
-        $("#result").html(res);
+        signify(res);
 
 
         $.ajax({
-          url: "https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=query&titles=" + noun + "&prop=images&format=json",
+          url: "https://simple.wikipedia.org/w/api.php?action=query&titles=" + noun + "&prop=images&format=json&origin=*&redirects=1",
           method: 'GET',
           dataType: 'json',
           async: true
@@ -26,7 +108,7 @@ function extract(noun) {
 
 
               $.ajax({
-                url: "https://crossorigin.me/http://en.wikipedia.org/w/api.php?action=query&titles=" + element.title + "&prop=imageinfo&iiprop=url&format=json",
+                url: "http://simple.wikipedia.org/w/api.php?action=query&titles=" + element.title + "&prop=imageinfo&iiprop=url&format=json&origin=*&redirects=1",
                 method: 'GET',
                 dataType: 'json',
                 async: true
@@ -34,8 +116,9 @@ function extract(noun) {
                 try {
                   url = img.query.pages[Object.keys(img.query.pages)[0]].imageinfo[0].url;
                   insert = ("<img src='" + url + "' style='display: inline; margin: auto; width: 50%; image-rendering:crisp-edges'>")
-                  console.log(insert)
-                  $("#result").append(insert);
+                  if (insert.indexOf(".svg") == -1 && insert.indexOf(".webm") == -1 && insert.indexOf(".ogv") == -1 && insert.indexOf(".ogg") == -1) {
+                    $("#img-result").append(insert);
+                  }
                 } catch(e){}
               });
 
--- js/functions.js
+++ js/functions.js
@@ -7,6 +7,23 @@ $(document).ready(function() {
  		return false;
 	});
 
+var modal = document.getElementById('myModal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 	$('.form-container').submit(function() {
 		if(!$('.form-container .search-field').val() || $('.form-container .search-field').val() == "Type search text here...") {
 			$('.popup').css({ opacity: 0 });
